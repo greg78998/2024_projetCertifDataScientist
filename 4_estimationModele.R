0575546494401d5922a35198ad5f39860c896076
GregHani <57009712+greg78998@users.noreply.github.com>
2024-04-21 19:33:00 +0200

    Add files via upload

--- 4_estimationModele.R
+++ 4_estimationModele.R
@@ -11,10 +11,14 @@ if (matricule == ""){
 
 # Chargement des utilisateurs 
 
-chargement_modeles <- FALSE
-forme_dt <- "simple"   # ou poly
+chargement_modeles <- TRUE
 nb_model <- 10
 
+forme_dt_ls <- c("simple","add","poly")  
+# SIMPLE : Pas de feature engineering (sauf retraitement)
+# ADD : Ajout de variables            (ajout de variables)
+# POLY :  polygon sur les variables météo
+# Interaction : encore à définir
 
 # ------------------------------------------------------------------------------ 
 
@@ -28,16 +32,21 @@ source(paste0(path_USER,"/pg_propre/","_before_libraries.R"))
 
 source(paste0(path_USER,"/pg_propre/","X3_MeF_predictionModels.R"))
 
-DB <- readRDS(paste0(path_data_vf,"/","base_postRET.RDS"))
+dt_placement <- readRDS(file = paste0(path_data_vf,"/","para_dt_placement.RDS"))
+interval_month <- readRDS( file = paste0(path_data_vf,"/","para_interval_month.RDS"))
+annee_nb <- readRDS( file = paste0(path_data_vf,"/","para_annee_nb.RDS"))
 
-if (forme_dt == "poly"){
-  DB_poly <- readRDS(paste0(path_data_vf,"/","base_precipitationcarre.RDS"))
-  DB <- DB %>% cbind(DB_poly)
-}
+forme_dt  <- paste(forme_dt_ls, collapse = "_") # pour les dénomination des modèles
 
 dt_placement <- readRDS(file = paste0(path_data_vf,"/","para_dt_placement.RDS"))
 interval_month <- readRDS( file = paste0(path_data_vf,"/","para_interval_month.RDS"))
 annee_nb <- readRDS( file = paste0(path_data_vf,"/","para_annee_nb.RDS"))
+
+DB <- readRDS(paste0(path_data_vf,"/",dt_placement,"_DB_postRET.RDS"))          # = forme simple
+
+source(paste0(path_USER,"/pg_propre/","Y1_assemblageTables.R"))
+
+
 
 # 2 | Remise en page du dataframe 
 
@@ -46,12 +55,15 @@ DB <- DB %>%
   rename(Y = top_defaillance) %>%
   select(-c(ea_ul)) # ligne qui va disparaître à terme
 
-set.seed(1234)
-data_split <- initial_split(DB, strata = Y, prop = 0.8)
+need <- TRUE # Pour assurer la reproductivite
+if (need){
+  set.seed(1234)
 
-training <- training(data_split) # data frame qui permet de faire le premier découpage
-test_set <- testing(data_split) # extraire le test set
-
+  data_split <- initial_split(DB, strata = Y, prop = 0.8)
+  
+  training <- training(data_split) # data frame qui permet de faire le premier découpage
+  test_set <- testing(data_split) # extraire le test set
+}
 
 # 3 | estimation de modèle sur l'ensemble du modèle
 
@@ -61,13 +73,15 @@ for (pho in 1:nb_model){
     
     if (pho < 3){
       print(paste0("Entrainement logit n°",pho, sep= ""))
-      logit_mod_vf <- glm(Y~.,data=training,family="binomial")
-      saveRDS(logit_mod_vf, file = paste0(path_data_vf,"/",forme_dt,"_vf_logit_n",pho,".RDS"))
+      logit_mod_vf <- glm(Y~.,data=training,family=binomial(link = "probit"))
+      saveRDS(logit_mod_vf, file = paste0(path_pg_models_save,"/",forme_dt,"_vf_logit_n",pho,".RDS"))
+      print("Ce modèle logit est sauvegardé")
     }
     
     print(paste0("Entrainement d'une forêt aléatoire n°",pho, sep= ""))
     rF_mod_vf <- ranger::ranger(as.factor(Y)~., data=training, probability = TRUE)
-    saveRDS(rF_mod_vf, file = paste0(path_data_vf,"/",forme_dt,"_vf_rf_n",pho,".RDS"))
+    saveRDS(rF_mod_vf, file = paste0(path_pg_models_save,"/",forme_dt,"_vf_rf_n",pho,".RDS"))
+    print("Ce modèle randomForest est sauvegardé")
   }
 }
 
@@ -83,24 +97,24 @@ for (pho in 1:nb_model){
   
   if (pho < 3){
     print("logit")
-    logit_mod_vf <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_vf_logit_n",pho,".RDS"))
+    logit_mod_vf <- readRDS(file = paste0(path_pg_models_save,"/",forme_dt,"_vf_logit_n",pho,".RDS"))
     DF_entrainement[, paste0("logit_",pho)] <- predict(logit_mod_vf,training, type = "response")
   }
 
   print("randomForest")
-  rF_mod <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_vf_rf_n",pho,".RDS"))
+  rF_mod <- readRDS(file = paste0(path_pg_models_save,"/",forme_dt,"_vf_rf_n",pho,".RDS"))
   DF_entrainement[, paste0("rf_",pho)] <- predict(rF_mod,training)$prediction[,2]
   
   print(paste0("Données jamais vu : n°",pho))
   
   if (pho < 3){
     print("logit")
-    logit_mod_vf <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_vf_logit_n",pho,".RDS"))
+    logit_mod_vf <- readRDS(file = paste0(path_pg_models_save,"/",forme_dt,"_vf_logit_n",pho,".RDS"))
     DF_test[, paste0("logit_",pho)] <- predict(logit_mod_vf,test_set, type = "response")
   }
   
   print("randomForest")
-  rF_mod <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_vf_rf_n",pho,".RDS"))
+  rF_mod <- readRDS(file = paste0(path_pg_models_save,"/",forme_dt,"_vf_rf_n",pho,".RDS"))
   DF_test[, paste0("rf_",pho)] <- predict(rF_mod,test_set)$prediction[,2]
 }
 
@@ -110,9 +124,9 @@ DF_test_aug <- add_var_model(para_db = DF_test, para_threshold = seuil)
 
 # test 
 saveRDS(DF_entrainement,
-        file = paste0(path_data_vf,"/","basesPREVISION_train",".RDS") )
+        file = paste0(path_data_vf,"/",dt_placement,'_',forme_dt,"pourSHINY_training",".RDS") )
 saveRDS(DF_test,
-        file = paste0(path_data_vf,"/","basesPREVISION_test",".RDS") )
+        file = paste0(path_data_vf,"/",dt_placement,'_',forme_dt,"pourSHINY_test",".RDS") )
 
 # Matrice de confusion
 table(DF_entrainement_aug$Y,DF_entrainement_aug$top_voteMajo)/dim(DF_entrainement_aug)[1]*100 # Un vote majoritaire
