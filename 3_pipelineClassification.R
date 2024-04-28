11deb0e50da883e06a3160caef4c4bd49607b3e4
jmigeon <159251869+jmigeon@users.noreply.github.com>
2024-04-28 13:15:00 +0200

    Avec VC et Hypertuning

--- 3_pipelineClassification.R
+++ 3_pipelineClassification.R
@@ -1,17 +1,18 @@
 # pipeline - Classification 
 
-matricule <- "N818398"
+matricule <- "X822385"
 
 if (matricule == "N818398") {
   path_USER <- paste0("C:/Users/",matricule,"/Desktop/projetBdF", sep = "")
 } 
-if (matricule == ""){
-  path_USER <- ""
+if (matricule == "X822385"){
+  path_USER <- "C:/Users/X822385/Desktop/DS/Projet"
 }
 
 
-chargement_modeles <- FALSE
+chargement_modeles <- TRUE
 forme_dt_ls <- c("simple","add","poly")  
+
 
   # SIMPLE : Pas de feature engineering (sauf retraitement)
   # ADD : Ajout de variables            (ajout de variables)
@@ -38,31 +39,25 @@ DB <- readRDS(paste0(path_data_vf,"/",dt_placement,"_DB_postRET.RDS"))          
 source(paste0(path_USER,"/pg_propre/","Y1_assemblageTables.R"))
 
 
-nb_bloc <- 2 
-
 # 1 | chargement des modèles pour la cross validation
 
 if (chargement_modeles == FALSE){
   
-  logit_mod <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_logit.RDS"))
+  logit_fitted_cv <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_logit_tuned.RDS"))
   
-  ridge_mod <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_ridge.RDS"))
-  lasso_mod <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_lasso.RDS"))
-  elasNet_mod <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_elasticNet.RDS"))
-  
-  rpart_mdl <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_arbre.RDS"))
-  
-  rF_mod1 <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_rf1.RDS"))
-  xgb_mod <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_xgb.RDS"))
+  ridge_fitted_cv <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_ridge_tuned.RDS"))
+  lasso_fitted_cv <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_lasso_tuned.RDS"))
+  elasNet_fitted_cv <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_elasticNet_tuned.RDS"))
+  rF_mod1_fitted_cv <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_rf1_tuned.RDS"))
+  xgb_fitted_cv <- readRDS(file = paste0(path_data_vf,"/",forme_dt,"_mod_xgb_tuned.RDS"))
 }
 
 
 DB <- DB %>% 
   select(-c(siren,dt)) %>% 
   rename(Y = top_defaillance) %>%
-  select(-c(ea_ul)) # ligne qui va disparaître à terme
-
-
+  select(-c(ea_ul,date_creation,region,nj_ret,ape_ret)) # pour l'instant, je retire toutes les quali mais à ajuster
+# summary(DB)
 
 
 # 2 | Mise en place du training + test
@@ -70,11 +65,12 @@ DB <- DB %>%
 # pour véirfier que ça tourne 
 #DB <- training(initial_split(DB, strata = Y, prop = 1))
 
+DB <- na.omit(DB) # ajout pour debugger
+
 need <- TRUE    # Pour assurer la reproductivité 
 if (need){
   set.seed(1234)
-  blocs <- sample(rep(1:nb_bloc, length(nrow(DB))))
-  
+
   data_split <- initial_split(DB, strata = Y, prop = 0.8)
   
   training <- training(data_split) # data frame qui permet de faire le premier découpage
@@ -86,89 +82,130 @@ if (need){
   eval_set <- testing(training_split)
 }
 
+# Recipe sur training (et pas sur train_set car on fait de la validation croisée)
+training$Y <- factor(training$Y)
+rec <- recipe(data = training, Y~.)
+class(rec)
+prep(rec)
+juice(prep(rec))
 
-tabPREV <- eval_set %>% select(Y)
 
-XXA <- model.matrix(Y~., data = train_set)    # Utilisation pour le ridge, elastic, lasso
-YYA <- train_set$Y
-XXT <- model.matrix(Y~., data = eval_set)
-YYT <- eval_set$Y
+# Ajout de la VC avec X blocs
+folds <- vfold_cv(training, v=5, strata=Y)
 
-# 3 | Mise en place
 
+# 3 | Mise en place (dont hypertuning à l'aide de la random grid search => pour grid je met à 5 mais la prod met à 20)
+
+### REGRESSION LOGISTIQUE (PAS D'HYPERTUNING POUR CE MODELE)
 if (chargement_modeles == TRUE){
-  logit_mod <- glm(Y~.,data=train_set,family="binomial")
-  saveRDS(logit_mod, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_logit.RDS"))
+log_mod <- logistic_reg(mode="classification", engine="glm") # voir si ça fonctionne avec glmnet
+  log_wf <- workflow()%>%
+    add_recipe(rec)%>%
+    add_model(log_mod)
+  log_fitted_cv <- log_wf %>%
+    tune_grid(
+      resamples = folds,
+      grid=5,
+      metrics = metric_set(accuracy, roc_auc)
+    )
+saveRDS(log_fitted_cv, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_log_tuned.RDS"))
+log_fitted_cv %>% collect_metrics()
+best_params_log <- show_best(log_fitted_cv, metric="roc_auc") # penalty=  ; roc_auc =
+best_params_log
 }
-tabPREV$logit <- predict(logit_mod, eval_set, type = 'response')
 
+### RANDOM FOREST
+rf_mod <- rand_forest(trees=tune(),min_n=tune())%>% # trees=nd d'arbres, min_n=profondeur d'arbre
+  set_engine('ranger')%>%
+  set_mode('classification')
+rf_wf <- workflow()%>%
+  add_recipe(rec)%>%
+  add_model(rf_mod)
+rf_fitted_cv <- rf_wf %>%
+  tune_grid(
+    resamples = folds, 
+    metrics = metric_set(accuracy, roc_auc),
+    grid=5
+  ) # la prof met un grid à 20, f_meas et recall
+saveRDS(rf_fitted_cv, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_rf1_tuned.RDS"))
+rf_fitted_cv %>% collect_metrics()
+rf_fitted_cv %>% collect_metrics() %>% filter (.metric =='roc_auc') %>% arrange (desc(mean)) # trees=1760 ; min_n =14 ; roc_auc =0,738 
+}
+
+### XGBOOST
 if (chargement_modeles == TRUE){
-  ridge_mod <- cv.glmnet(XXA,YYA,alpha=0,family="binomial", type.measure = "auc")
-  saveRDS(ridge_mod, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_ridge.RDS"))
+xg_mod <- boost_tree(mtry=tune(), min_n=tune())%>% #min_n=profondeur d'arbre, mtry=nb de feuilles
+  set_engine('xgboost')%>%
+  set_mode('classification')
+xg_wf <- workflow()%>%
+  add_recipe(rec)%>%
+  add_model(xg_mod)
+xg_fitted_cv <- xg_wf %>%
+  tune_grid(
+    resamples = folds,
+    grid=5,
+    metrics = metric_set(accuracy, roc_auc)
+  )
+saveRDS(xg_fitted_cv, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_xgb_tuned.RDS"))
+xg_fitted_cv %>% collect_metrics()
+xg_fitted_cv %>% collect_metrics() %>% filter (.metric =='roc_auc') %>% arrange (desc(mean)) # mtry=114 ; min_n=19 ; roc_auc = 0,781
 }
-tabPREV$ridge <- predict(ridge_mod,XXT,s="lambda.min",type="response")
 
+### LASSO
 if (chargement_modeles == TRUE){
-  lasso_mod <- cv.glmnet(XXA,YYA,alpha=1,family="binomial", type.measure = "auc")
-  saveRDS(lasso_mod, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_lasso.RDS"))
+lasso_mod <- logistic_reg(mode="classification", engine="glmnet", penalty=tune(), mixture=1) 
+lasso_wf <- workflow()%>%
+  add_recipe(rec)%>%
+  add_model(lasso_mod)
+lasso_fitted_cv <- lasso_wf %>%
+  tune_grid(
+    resamples = folds,
+    grid=5,
+    metrics = metric_set(accuracy, roc_auc)
+  )
+saveRDS(lasso_fitted_cv, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_lasso_tuned.RDS"))
+lasso_fitted_cv %>% collect_metrics()
+best_params_lasso <- show_best(lasso_fitted_cv, metric="roc_auc") # penalty=4.81e- 5  ; roc_auc = 0.686
+best_params_lasso
 }
-tabPREV$lasso <- predict(lasso_mod,XXT,s="lambda.min",type="response")
 
+### RIDGE
 if (chargement_modeles == TRUE){
-  elasNet_mod <- cv.glmnet(XXA,YYA,alpha=0.5,family="binomial", type.measure = "auc")
-  saveRDS(elasNet_mod, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_elasticNet.RDS"))
+ridge_mod <- logistic_reg(mode="classification", engine="glmnet", penalty=tune(), mixture=0) 
+ridge_wf <- workflow()%>%
+  add_recipe(rec)%>%
+  add_model(ridge_mod)
+ridge_fitted_cv <- ridge_wf %>%
+  tune_grid(
+    resamples = folds,
+    grid=5,
+    metrics = metric_set(accuracy, roc_auc, f_meas,recall)
+  )
+saveRDS(ridge_fitted_cv, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_ridge_tuned.RDS"))
+ridge_fitted_cv %>% collect_metrics()
+ridge_fitted_cv %>% collect_metrics() %>% filter (.metric =='roc_auc') %>% arrange (desc(mean)) # penalty=1.24e-10 ; roc_auc = 0,672
 }
-tabPREV$elasticNet <- predict(elasNet_mod,XXT,s="lambda.min",type="response")
 
+### ELASTICNET (IL ME RESISTE MAIS JE SUIS DESSUS)
 if (chargement_modeles == TRUE){
-  rpart_mdl <- rpart::rpart(as.factor(Y)~., data=train_set)
-  saveRDS(rpart_mdl, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_arbre.RDS"))
-}
-tabPREV$arbre <- predict(rpart_mdl,eval_set,type="prob")[,2]
-
-if (chargement_modeles == TRUE){
-  rF_mod1 <- ranger::ranger(as.factor(Y)~., data=train_set, probability = TRUE)
-  saveRDS(rF_mod1, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_rf1.RDS"))
-}
-tabPREV$rF_mod1 <- predict(rF_mod1,eval_set)$prediction[,2]
-
-xgb_train = xgb.DMatrix(data = XXA, label = YYA)      # Utilisation pour le xgb
-xgb_test = xgb.DMatrix(data = XXT, label = YYT)
-
-if (chargement_modeles == TRUE){
-  xgb_cv <- xgb.cv(data=xgb_train,nrounds=100,max_depth=2,nfold=10,verbose=TRUE,eta=0.1)
-  iteropt <- which.min(xgb_cv$evaluation_log$test_rmse_mean)
-  
-  xgb_mod <- xgboost(data = xgb_train, max.depth = 2, 
-                     nrounds = iteropt, eta=0.1,
-                     verbose = TRUE, 
-                     objective = "binary:logistic")
-  saveRDS(xgb_mod, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_xgb.RDS"))
-}
-tabPREV$xgb_mod <- predict(xgb_mod,xgb_test)
-
-
-
-
-
-
-accuracy <- function(X,Y,seuil=0.5){
-  Xc=X*0
-  Xc[X>seuil] <- 1
-  sum(Xc==Y)/length(Y)*100
+elasticnet_mod <- logistic_reg(mode="classification", engine="glmnet", penalty=tune(), mixture=tune()) 
+elasticnet_wf <- workflow()%>%
+  add_recipe(rec)%>%
+  add_model(elasticnet_mod)
+grid <- expand.grid(
+  penalty=c(0,0.5,1),
+  mixture=seq(0,1, by=0.1)
+)
+elasticnet_fitted_cv <- elasticnet_wf %>%
+  tune_grid(
+    resamples = folds,
+    grid=grid,
+    metrics = metric_set(roc_auc)
+  )
+saveRDS(elasticnet_fitted_cv, file = paste0(path_pg_models_save,"/",forme_dt,"_mod_elasticnet_tuned.RDS"))
+elasticnet_fitted_cv %>% collect_metrics()
+elasticnet_fitted_cv %>% collect_metrics() %>% filter (.metric =='roc_auc') %>% arrange (desc(mean)) # penalty= ; roc_auc = 
 }
 
-recall <- function(X,Y,seuil=0.5){
-  Xc=X*0
-  Xc[X>seuil] <- 1
-  
-  vrai_positif <- sum(Xc == 1 & Y == 1)
-  positif_real <- sum(Y==1)
-  
-  vrai_positif / positif_real*100
-}
-
-apply(X = tabPREV,2,accuracy,Y=tabPREV$Y)
-apply(X = tabPREV,2,accuracy,Y=tabPREV$Y,seuil = 0.001)
-apply(X = tabPREV,2,recall,Y=tabPREV$Y,seuil = 0.001)
+# 4 | Comparaison des modèles (reste à faire)
 
