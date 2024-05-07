44f73a040d6f15367df24ea5ae95795599a41318
GregHani <57009712+greg78998@users.noreply.github.com>
2024-05-07 10:13:40 +0200

    MeF - final fit

--- 4_estimationModele.R
+++ 4_estimationModele.R
@@ -3,7 +3,7 @@
 # NECESSITE DE LANCER LE PROGRAMME 3 AU PREALABLE #
 ###################################################
 
-matricule <- "X822385"
+matricule <- "N818398"
 
 if (matricule == "N818398") {
   path_USER <- paste0("C:/Users/",matricule,"/Desktop/projetBdF", sep = "")
@@ -38,146 +38,125 @@ source(paste0(path_USER,"/pg_propre/","_before_libraries.R"))
 # 1 | chargement de la table + chargement des paramètres
 
 source(paste0(path_USER,"/pg_propre/","X3_MeF_predictionModels.R"))
+source(paste0(path_USER,"/pg_propre/","X6_assemblage_table.R"))
 
-dt_placement <- readRDS(file = paste0(path_data_vf,"/","para_dt_placement.RDS"))
-interval_month <- readRDS( file = paste0(path_data_vf,"/","para_interval_month.RDS"))
-annee_nb <- readRDS( file = paste0(path_data_vf,"/","para_annee_nb.RDS"))
 
 dt_placement <- readRDS(file = paste0(path_data_vf,"/","para_dt_placement.RDS"))
 interval_month <- readRDS( file = paste0(path_data_vf,"/","para_interval_month.RDS"))
 annee_nb <- readRDS( file = paste0(path_data_vf,"/","para_annee_nb.RDS"))
 
 # ATTENTION, dépend de la forme choisie au programme 3 (où on fait les retraitements de DB)
-DB <- readRDS(paste0(path_data_vf,"/",dt_placement,"_DB_postRET.RDS")) 
+
+DB <- X6_construction_base(forme_dt_ls,para_succ_nb_periode = 3)
+forme_dt  <- paste(forme_dt_ls, collapse = "_")
+
+DB <- DB %>% 
+  select(-c(siren,dt,ea_ul)) %>% # je retire "ea_ul" qui n'a plus d'utilité (var quali : region, nj_ret, ape_ret, ent_age_ret)
+  rename(Y = top_defaillance) %>% 
+  mutate(Y = factor(Y))
 
 
+need <- TRUE    # Pour assurer la reproductivité 
+if (need){
+  set.seed(1234)
+  
+  data_split <- initial_split(DB, strata = Y, prop = 0.8)
+  
+  training <- training(data_split) # data frame qui permet de faire le premier découpage
+  test_set <- testing(data_split) # extraire le test set
+  
+  training_split <- initial_split(training, strata = Y, prop = 0.8)
+  
+  train_set <- training(training_split)
+  eval_set <- testing(training_split)
+}
+
+rec <- recipe(data = training, Y~.) %>%
+  step_dummy(region) %>%
+  step_dummy(nj_ret) %>%
+  step_dummy(ape_ret) %>%
+  step_dummy(ent_age_ret)
 
 # 2 | CALCUL DES PREDICTIONS SUR TRAINING (DF_ENTRAINEMENT) ET SUR TEST (DF_TEST) + GESTION DU DE NB_MODEL
-training$Y <- factor(training$Y)
-test_set$Y <- factor(test_set$Y)
-train_set$Y <- factor(train_set$Y)
+
 
 ##### XGBOOST
 results_list_xgb <- list()
 DF_entrainement <- data.frame(Y=training$Y)
 DF_test <- data.frame(Y=test_set$Y)
+
+xgb_mod <- boost_tree(mtry=81, min_n=23) %>% #min_n=profondeur d'arbre, mtry=nb de feuilles
+  set_engine('xgboost')%>%
+  set_mode('classification')
+log_mod <- logistic_reg()%>%
+  set_engine('glm')%>%
+  set_mode('classification')
+
+
 for (pho in 1:nb_model){
-  print(paste0("numero :",pho))
+  
+  print(paste0("tour de boucle :",pho))
+
   if (chargement_modeles == TRUE){
+    
     print(paste0("Entrainement XGBOOST n°",pho, sep= ""))
+  
     set.seed(pho)
-    grid_xgb <- data.frame(mtry = 81, min_n = 23)
-    
-    wf_set_xgb <- workflow_set(
-      preproc = list(basic = rec),
-      models = list(xgb = xgb_mod))
-    
-    res_wf_set_xgb <- wf_set_xgb %>%
-      workflow_map(
-        resamples = folds,
-        metrics = metric_set(accuracy, roc_auc, recall),
-        grid = grid_xgb,
-        control=control_grid(save_pred=TRUE),
-        verbose = TRUE)
- 
-    saveRDS(res_wf_set_xgb, file = paste0(path_pg_models_save,"/",forme_dt,"_vf_xgb_n",pho,".RDS"))
-    print("Ce modèle XG Boost est sauvegardé")
-    
-    predictions_entrainement <- res_wf_set_xgb %>% collect_predictions()
-    results_list_xgb[[pho]] <- list(predictions=predictions_entrainement)
-    DF_entrainement[,paste0("xgb_",pho)] <- results_list_xgb[[pho]]$predictions[,c(".pred_1")]
-    
-    best_model_xgb <- 
-      res_wf_set_xgb %>%
-      extract_workflow_set_result('basic_xgb')%>%
-      select_best(metric='roc_auc')
 
-    last_fit_xgb <- res_wf_set_xgb %>%
-      extract_workflow("basic_xgb")%>%
-      finalize_workflow(best_model_xgb) %>%
-      last_fit(split=data_split, metrics=metric_set(accuracy, roc_auc))
-    
-    predictions_test <- last_fit_xgb %>% collect_predictions()
-    results_list_xgb[[pho]] <- list(predictions=predictions_test)
-    DF_test[,paste0("xgb_",pho)] <- results_list_xgb[[pho]]$predictions[,c(".pred_1")]
-    }
-}
-
-
-
-###### RANDOM_FOREST (JE NE L'AI PAS ENCORE LANCE DEPUIS QUE LE CODE A CHANGE... PREND DU TEMPS)
-
-wf_set_rf <- workflow_set(
-  preproc = list(basic = rec),
-  models = list(rf = rf_mod) # 
-)
-
-res_wf_set_rf_vf <- wf_set_rf %>%
-  workflow_map(
-    resamples = folds,
-    metrics = metric_set(accuracy, roc_auc, recall),
-    grid = grid_rf,
-    verbose = TRUE
-  )
-
-best_model_rf_vf <- 
-  res_wf_set_rf_vf %>%
-  extract_workflow_set_result('basic_rf')%>%
-  select_best(metric='roc_auc')
-
-last_fit_rf_vf <- res_wf_set_rf_vf %>%
-  extract_workflow("basic_rf")%>%
-  finalize_workflow(best_model_rf_vf) %>%
-  last_fit(split=data_split)
-
-last_fit_rf_vf%>%collect_metrics()
-last_fit_rf_vf%>%collect_predictions()
-
-
-results_list_rf <- list()
-#DF_entrainement <- data.frame(Y=training$Y)
-#DF_test <- data.frame(Y=test_set$Y)
-for (pho in 1:nb_model){
-  print(paste0("numero :",pho))
-  if (chargement_modeles == TRUE){
-    print(paste0("Entrainement RANDOM_FOREST n°",pho, sep= ""))
-    set.seed(pho)
-    grid_rf <- data.frame(trees = 500, min_n = 29) # on bloque à 500, pas encore fait retourner l'hypertuning où on bloque trees à 500 
-    
-    wf_set_rf <- workflow_set(
-      preproc = list(basic = rec),
-      models = list(xgb = rf_mod))
-    
-    res_wf_set_rf <- wf_set_rf %>%
-      workflow_map(
-        resamples = folds,
-        metrics = metric_set(accuracy, roc_auc, recall),
-        grid = grid_rf,
-        control=control_grid(save_pred=TRUE),
-        verbose = TRUE)
-    
-    saveRDS(res_wf_set_rf, file = paste0(path_pg_models_save,"/",forme_dt,"_vf_rf_n",pho,".RDS"))
-    print("Ce modèle RANDOM_FOREST est sauvegardé")
-    
-    predictions_entrainement <- res_wf_set_rf %>% collect_predictions()
-    results_list_rf[[pho]] <- list(predictions=predictions_entrainement)
-    DF_entrainement[,paste0("rf_",pho)] <- results_list_rf[[pho]]$predictions[,c(".pred_1")]
-    
-    best_model_rf <- 
-      res_wf_set_rf %>%
-      extract_workflow_set_result('basic_rf')%>%
-      select_best(metric='roc_auc')
-    
-    last_fit_rf <- res_wf_set_rf %>%
-      extract_workflow("basic_rf")%>%
-      finalize_workflow(best_model_rf) %>%
-      last_fit(split=data_split, metrics=metric_set(accuracy, roc_auc))
-    
-    predictions_test <- last_fit_rf %>% collect_predictions()
-    results_list_rf[[pho]] <- list(predictions=predictions_test)
-    DF_test[,paste0("rf_",pho)] <- results_list_rf[[pho]]$predictions[,c(".pred_1")]
+    xgb_fit <- workflow() %>% 
+      add_recipe(rec) %>%
+      add_model(xgb_mod) %>%
+      fit(training)
+  
+    saveRDS(xgb_fit, file = paste0(path_pg_models_save,"/",forme_dt,"_vf_xgb_n",pho,".RDS"))
   }
+  
+  # ----------------------------------------------------------------------------
+  
+  DF_entrainement[,paste("xgb_mod_",pho, sep = "")] <- predict(xgb_fit, training, type = "prob")[2]
+  DF_test[,paste("xgb_mod_",pho, sep = "")] <- predict(xgb_fit, test_set, type = "prob")[2]
+
+  if (pho < 2){
+    if (chargement_modeles == TRUE){
+    
+    print(paste0("Entrainement Logit n°",pho, sep= ""))
+    
+    set.seed(pho)
+    
+    logit_fit <- workflow() %>% 
+      add_recipe(rec) %>%
+      add_model(log_mod) %>%
+      fit(training)
+    
+    saveRDS(logit_fit, file = paste0(path_pg_models_save,"/",forme_dt,"_vf_logit_n",pho,".RDS"))
+    }
+    DF_entrainement[,paste("logit_mod_",pho, sep = "")] <- predict(logit_fit, training, type = "prob")[2]
+    DF_test[,paste("logit_mod_",pho, sep = "")] <- predict(logit_fit, test_set, type = "prob")[2]
+  }
+  
+  # ----------------------------------------------------------------------------
+  
+  if (chargement_modeles == TRUE){
+    
+    print(paste0("Entrainement randomForest n°",pho, sep= ""))
+    
+    set.seed(pho)
+    
+    rf_mod_fit <- ranger(Y ~ ., data=training, probability = TRUE)
+    
+    saveRDS(rf_mod_fit, file = paste0(path_pg_models_save,"/",forme_dt,"_vf_random_forest_n",pho,".RDS"))
+  }
+  
+  # ----------------------------------------------------------------------------
+  
+  DF_entrainement[,paste("rf_mod_",pho, sep = "_")] <- predict(rf_mod_fit, training)[2]
+  DF_test[,paste("rf_mod",pho, sep = "_")] <- predict(rf_mod_fit, test_set)$predictions[,2]
+  
+  
 }
+
+
+
 
 ###### Sauvegarde des DF_entrainement et DF_test pour l'application SHINY
 saveRDS(DF_entrainement,
@@ -187,13 +166,16 @@ saveRDS(DF_test,
 
 
 
+
+
+
 # 3 | DIVERS ANALYSES SUR DF_TEST
 # DF_test <- readRDS( file = paste0(path_data_vf,"/",dt_placement,'_',forme_dt,"basesPREVISION_test",".RDS"))
 
 # Courbes ROC
 library(pROC)
 roc_data <- roc(Y~.,data=DF_test)
-plot(roc_data$xgb_1)
+plot(roc_data$xgb_mod_1)
 
 roc_data_2 <- roc(DF_test$Y,DF_test$xgb_1)
 optimal_threshold_sensitivity <- coords(roc_data_2, "best", ret="threshold")
