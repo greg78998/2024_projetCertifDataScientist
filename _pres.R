4ee0bf1b6ebbaa8beacc6172f659b8c4fcba0d0a
jmigeon <159251869+jmigeon@users.noreply.github.com>
2024-05-10 17:42:51 +0200

    Ajout "Calibrage" + utilisation des vrais données

--- _pres.R
+++ _pres.R
@@ -1,12 +1,12 @@
 ## Avant le lancement de l'application 
 
-matricule <- "N818398"
+matricule <- "X822385"
 
 if (matricule == "N818398") {
   path_USER <- paste0("C:/Users/",matricule,"/Desktop/projetBdF", sep = "")
 } 
-if (matricule == ""){
-  path_USER <- ""
+if (matricule == "X822385"){
+  path_USER <- "C:/Users/X822385/Desktop/DS/Projet"
 }
 
 # 0 | chargement des libraries  -----
@@ -35,20 +35,23 @@ DB <- readRDS(paste0(path_data_vf,"/","base_postRET.RDS"))
 DB_defaillance <- DB %>% filter(top_defaillance ==1 )
 
 # On vient chercher la table de prévision
-n<- 100000
-pourTravaux <- data.frame(Y = as.numeric(sample(c(0:10), size = n, replace=TRUE)>9))
-
-for (ii in 1:10){
-  pourTravaux[,paste0("rf_",ii)] <- rnorm(n = n, mean = 0.1,sd = 0.05)
-  pourTravaux[,paste0("xgb_",ii)] <- rnorm(n = n, mean = 0.1,sd = 0.05)
-  pourTravaux[,paste0("logit_",ii)] <- rnorm(n = n, mean = 0.1,sd = 0.05)
-}
-
-DF_entrainement <- pourTravaux[1:n*0.8+1,]
-DF_test <- pourTravaux[n*0.8+1:dim(pourTravaux)[1],]
+#n<- 100000
+#pourTravaux <- data.frame(Y = as.numeric(sample(c(0:10), size = n, replace=TRUE)>9))
+#
+#for (ii in 1:10){
+# pourTravaux[,paste0("rf_",ii)] <- rnorm(n = n, mean = 0.1,sd = 0.05)
+# pourTravaux[,paste0("xgb_",ii)] <- rnorm(n = n, mean = 0.1,sd = 0.05)
+# pourTravaux[,paste0("logit_",ii)] <- rnorm(n = n, mean = 0.1,sd = 0.05)
+#}
+#
+#DF_entrainement <- pourTravaux[1:n*0.8+1,]
+#DF_test <- pourTravaux[n*0.8+1:dim(pourTravaux)[1],]
 
 #DF_entrainement <- readRDS(file = paste0(path_data_vf,"/","basesPREVISION_train",".RDS") )
 #DF_test <- readRDS(file = paste0(path_data_vf,"/","basesPREVISION_test",".RDS") )
+
+DF_entrainement <- readRDS(file = paste0(path_data_vf,"/",dt_placement,'_',forme_dt,'_',"basesPREVISION_train",".RDS"))
+DF_test <- readRDS(file = paste0(path_data_vf,"/",dt_placement,'_',forme_dt,'_',"basesPREVISION_test",".RDS") )
 
 ls_label <- c("Non en difficulté", "En difficulté")
 
